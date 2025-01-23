fe1a362fe848559e16cba7066145883668f79823
Dhireen Kumar Rajak <iamdeerajak@gmail.com>
2025-01-23 19:32:48 +0530

    Getting Started

---
+++ 2_simple_values/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1+1)
+	fmt.Println("Hello, World!")
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(1.0)
+	fmt.Println(7/3)
+	fmt.Println(7.0/3.0)
+}
---
+++ 3_variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "deez"
+	// var name = "deez"
+	// var isAdult bool = true
+	// var age int = 21
+
+	name := "deez"
+
+	fmt.Println(name)
+}
