1750f43c3da0fb879bbe4ccca60a11f42a71dde4
Dhireen Kumar Rajak <iamdeerajak@gmail.com>
2025-01-23 19:37:04 +0530

    constant

---
+++ 4_constants/constants.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const age = 90
+
+var name = "godeez"
+
+func main() {
+	const name string = "deez"
+	const age = 80
+	fmt.Println(name)
+
+	const (
+		port = 5000
+		host = "localhost"
+	)
+
+	// const sub string
+
+	// sub = "sub"
+}
